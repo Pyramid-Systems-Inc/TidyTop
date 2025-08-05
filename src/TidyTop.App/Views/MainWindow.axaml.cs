8bb84e194774ce766a5248be18b85750e8c07b2c
Omar Aglan <omar.aglan91@yahoo.com>
2025-08-05 15:50:04 +0300

    Refactor MainWindow.axaml and MainWindow.axaml.cs for improved event handling and data binding

--- src/TidyTop.App/Views/MainWindow.axaml.cs
+++ src/TidyTop.App/Views/MainWindow.axaml.cs
@@ -63,21 +63,18 @@ public partial class MainWindow : Window
         ExtendClientAreaTitleBarHeightHint = -1;
     }
 
-    private void OnWindowPointerPressed(object sender, PointerPressedEventArgs e)
+    private void OnWindowPointerPressed(object? sender, PointerPressedEventArgs e)
     {
         var point = e.GetCurrentPoint(this);
         
         // Right-click to show/hide control panel
         if (point.Properties.IsRightButtonPressed)
         {
-            if (ViewModel != null)
-            {
-                ViewModel.ToggleControlPanelCommand.Execute(Unit.Default);
-            }
+            ViewModel?.ToggleControlPanelCommand.Execute(Unit.Default);
         }
     }
 
-    private void OnWindowPointerMoved(object sender, PointerEventArgs e)
+    private void OnWindowPointerMoved(object? sender, PointerEventArgs e)
     {
         if (!_isDragging) return;
         
@@ -110,14 +107,14 @@ public partial class MainWindow : Window
         }
     }
 
-    private void OnWindowPointerReleased(object sender, PointerReleasedEventArgs e)
+    private void OnWindowPointerReleased(object? sender, PointerReleasedEventArgs e)
     {
         _isDragging = false;
         _draggedFence = null;
         _draggedIcon = null;
     }
 
-    private void OnKeyDown(object sender, KeyEventArgs e)
+    private void OnKeyDown(object? sender, KeyEventArgs e)
     {
         // Global keyboard shortcuts
         switch (e.Key)
@@ -150,184 +147,6 @@ public partial class MainWindow : Window
                 }
                 break;
         }
-    }
-
-    private void OnFenceHeaderMouseDown(object sender, PointerPressedEventArgs e)
-    {
-        if (sender is Border border && border.Tag is Fence fence)
-        {
-            var point = e.GetCurrentPoint(this);
-            
-            if (point.Properties.IsLeftButtonPressed)
-            {
-                _isDragging = true;
-                _dragStartPoint = point.Position;
-                _draggedFence = fence;
-                
-                // Bring fence to front
-                if (ViewModel?.Fences.Contains(fence) == true)
-                {
-                    ViewModel.Fences.Remove(fence);
-                    ViewModel.Fences.Add(fence);
-                }
-            }
-            else if (point.Properties.IsRightButtonPressed)
-            {
-                // Show context menu for fence
-                ShowFenceContextMenu(fence, point.Position);
-            }
-        }
-    }
-
-    private void OnIconPointerPressed(object sender, PointerPressedEventArgs e)
-    {
-        if (sender is Border border && border.Tag is DesktopIcon icon)
-        {
-            var point = e.GetCurrentPoint(this);
-            
-            if (point.Properties.IsLeftButtonPressed)
-            {
-                if (e.ClickCount == 2)
-                {
-                    // Double-click to launch icon
-                    LaunchIcon(icon);
-                }
-                else
-                {
-                    // Start dragging
-                    _isDragging = true;
-                    _dragStartPoint = point.Position;
-                    _draggedIcon = icon;
-                }
-            }
-            else if (point.Properties.IsRightButtonPressed)
-            {
-                // Show context menu for icon
-                ShowIconContextMenu(icon, point.Position);
-            }
-        }
-    }
-
-    private void ShowFenceContextMenu(Fence fence, Point position)
-    {
-        var contextMenu = new ContextMenu();
-        
-        // Edit fence
-        var editItem = new MenuItem { Header = "Edit Fence" };
-        editItem.Click += (s, e) => EditFence(fence);
-        contextMenu.Items.Add(editItem);
-        
-        // Delete fence
-        var deleteItem = new MenuItem { Header = "Delete Fence" };
-        deleteItem.Click += (s, e) => DeleteFence(fence);
-        contextMenu.Items.Add(deleteItem);
-        
-        // Separator
-        contextMenu.Items.Add(new Separator());
-        
-        // Bring to front
-        var frontItem = new MenuItem { Header = "Bring to Front" };
-        frontItem.Click += (s, e) => BringFenceToFront(fence);
-        contextMenu.Items.Add(frontItem);
-        
-        // Send to back
-        var backItem = new MenuItem { Header = "Send to Back" };
-        backItem.Click += (s, e) => SendFenceToBack(fence);
-        contextMenu.Items.Add(backItem);
-        
-        // Open context menu
-        contextMenu.Open(this);
-    }
-
-    private void ShowIconContextMenu(DesktopIcon icon, Point position)
-    {
-        var contextMenu = new ContextMenu();
-        
-        // Open icon
-        var openItem = new MenuItem { Header = "Open" };
-        openItem.Click += (s, e) => LaunchIcon(icon);
-        contextMenu.Items.Add(openItem);
-        
-        // Properties
-        var propertiesItem = new MenuItem { Header = "Properties" };
-        propertiesItem.Click += (s, e) => ShowIconProperties(icon);
-        contextMenu.Items.Add(propertiesItem);
-        
-        // Separator
-        contextMenu.Items.Add(new Separator());
-        
-        // Remove from fence
-        var removeItem = new MenuItem { Header = "Remove from Fence" };
-        removeItem.Click += (s, e) => RemoveIconFromFence(icon);
-        contextMenu.Items.Add(removeItem);
-        
-        // Open context menu
-        contextMenu.Open(this);
-    }
-
-    private void EditFence(Fence fence)
-    {
-        // Implementation for fence editing dialog
-        // This would open a dialog to edit fence properties
-    }
-
-    private void DeleteFence(Fence fence)
-    {
-        if (ViewModel?.Fences.Contains(fence) == true)
-        {
-            ViewModel.Fences.Remove(fence);
-        }
-    }
-
-    private void BringFenceToFront(Fence fence)
-    {
-        if (ViewModel?.Fences.Contains(fence) == true)
-        {
-            ViewModel.Fences.Remove(fence);
-            ViewModel.Fences.Add(fence);
-        }
-    }
-
-    private void SendFenceToBack(Fence fence)
-    {
-        if (ViewModel?.Fences.Contains(fence) == true)
-        {
-            ViewModel.Fences.Remove(fence);
-            ViewModel.Fences.Insert(0, fence);
-        }
-    }
-
-    private void LaunchIcon(DesktopIcon icon)
-    {
-        try
-        {
-            if (!string.IsNullOrEmpty(icon.FullPath))
-            {
-                var process = new System.Diagnostics.Process
-                {
-                    StartInfo = new System.Diagnostics.ProcessStartInfo
-                    {
-                        FileName = icon.FullPath,
-                        UseShellExecute = true
-                    }
-                };
-                process.Start();
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Failed to launch icon: {ex.Message}");
-        }
-    }
-
-    private void ShowIconProperties(DesktopIcon icon)
-    {
-        // Implementation for icon properties dialog
-    }
-
-    private void RemoveIconFromFence(DesktopIcon icon)
-    {
-        // Implementation for removing icon from fence
     }
 
     protected override void OnOpened(EventArgs e)
